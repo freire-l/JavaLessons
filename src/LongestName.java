47375a64ff90327587e843b537285cb4b62261e4
Luis Freire <frebermu9@gmail.com>
2022-10-14 19:41:29 -0500

    Homework 6

--- src/LongestName.java
+++ src/LongestName.java
@@ -1,2 +1,24 @@
+import javax.swing.*;
+
 public class LongestName {
+    public static void main(String[] args) {
+        String firstName = JOptionPane.showInputDialog(null, "Enter the first full name");
+        String secondtName = JOptionPane.showInputDialog(null, "Enter the second full name");
+        String thirdName = JOptionPane.showInputDialog(null, "Enter the third full name");
+
+        String result = "";
+
+        if(firstName.split(" ")[0].length() > secondtName.split(" ")[0].length()){
+            result = firstName;
+        } else if (secondtName.split(" ")[0].length() > thirdName.split(" ")[0].length()) {
+            result = secondtName;
+        }
+        if(thirdName.split(" ")[0].length() > result.split(" ")[0].length()){
+            result = thirdName;
+        }
+
+        String outMessage = result + " has the longest name";
+
+        JOptionPane.showMessageDialog(null, outMessage);
+    }
 }
